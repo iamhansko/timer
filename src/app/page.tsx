0d407b40777df9d797e4d24d77c0475bdd0553ff
iamhansko <iamhansko@kakao.com>
2024-03-14 23:53:17 +0900

    feat: CountDown using Query Params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
+  const searchParams = useSearchParams()
+  const timeParam = searchParams.get('time') || '60'
+  const [time, setTime] = useState(parseInt(timeParam))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (time > 0) setTime(time - 1)
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [time])
+
   return (
     <main className={styles.main}>
-      SKKU Timer
+      <div className={`${styles.countDown} ${time>0 ? styles.active : styles.inactive} `}>
+        <div>{time}</div>
+      </div>
     </main>
   );
 }
