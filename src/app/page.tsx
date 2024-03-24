d7ad74c5a33091bcabe5e23691fe1a0dfd51fc1e
iamhansko <iamhansko@kakao.com>
2024-03-24 23:33:23 +0900

    fix: Query Param date Format YYYYMMDD -> YYYYMMDDhhmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const searchParams = useSearchParams()
   // const timeParam = parseInt(searchParams.get('time') ? searchParams.get('time') as string : '60')
   const isNumber = /\d+/
-  const isDate = /\d{8}/
+  const isDate = /\d{12}/
   const hour = searchParams.get('h') && isNumber.test(searchParams.get('h') as string) ? searchParams.get('h') as string : '0'
   const minute = searchParams.get('m') && isNumber.test(searchParams.get('m') as string) ? searchParams.get('m') as string : '0'
   const second = searchParams.get('s')  && isNumber.test(searchParams.get('s') as string) ? searchParams.get('s') as string : '0'
@@ -18,7 +18,7 @@ export default function Home() {
   const [time, setTime] = useState(timeParam)
 
   const dateParam = searchParams.get('date') && isDate.test(searchParams.get('date') as string) ? searchParams.get('date') as string : '0'
-  const dday = dateParam ? new Date(parseInt(dateParam.slice(0,4)), parseInt(dateParam.slice(4,6))-1, parseInt(dateParam.slice(6,8))) : new Date()
+  const dday = dateParam ? new Date(parseInt(dateParam.slice(0,4)), parseInt(dateParam.slice(4,6))-1, parseInt(dateParam.slice(6,8)), parseInt(dateParam.slice(8,10)), parseInt(dateParam.slice(10,12))) : new Date()
   const [today, setToday] = useState(dday)
   const leftTime = dateParam && dday && today ? Math.floor((dday.getTime() - today.getTime()) / 1000) : 0; 
 
